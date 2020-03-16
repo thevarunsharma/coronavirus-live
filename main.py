4df9aa55ed33d3b1f9e0e6bbb0b143d0680420d9
thevarunsharma <varunsharma123.del@gmail.com>
2020-03-17 00:15:46 +0530

    main flask app

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_apscheduler import APScheduler
+from updater import get_update
+from pickle import load
+
+class Config:
+    SCHEDULER_API_ENABLED = True
+
+scheduler = APScheduler()
+
+@scheduler.task('cron', id='data_fetch', minute='*/15')
+def scheduled_data_fetch():
+    get_update()
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+
+@app.route("/")
+def handle():
+    with open("stats/data.pickle", "rb") as fh:
+        data = load(fh)
+    with open("stats/world.pickle", "rb") as fh:
+        world = load(fh)
+    with open("stats/up-date.txt", "r") as fh:
+        last_updated = fh.read()
+    return render_template("home.html", data=data, world=world, last_updated=last_updated)
+
+
+scheduler.init_app(app)
+scheduler.start()
+
+if __name__=="__main__":
+    app.run(debug=True)
